8e3cbafa5a54c62d0d8db24a8ea822f1ac682e6f
Anshul <53268875+Anshul-Dagar@users.noreply.github.com>
2024-09-23 12:29:54 +0000

    Tags fetched from supabase

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
-import Image from "next/image";
+import Link from 'next/link'
 import { supabase } from "@/lib/supabaseClient";
+import { Separator } from "@/components/ui/separator"
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 export default async function Home() {
 
@@ -9,15 +17,29 @@ export default async function Home() {
     return <p>Failed to load articles</p>;
   }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] item-center justify-items-center min-h-screen p-6 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-2xl font-bold mb-4 ">News Article</h1>
+        <h1 className="text-2xl font-bold mb-4 ">News Articles</h1>
+
         <ul clsssName="space-y-4">
           {data?.map((article)=> (
-            <li key ={article.id}>
-              <a href={article.Link} className="text-blue-500 hover:underline">
-                {article.Title}
-              </a>
+            <li key ={article.id} className="my-4">
+              <Card>
+            <CardHeader>
+              <CardTitle>
+                    <Link href={article.Link} target="_blank" className="hover:underline text-justify">
+                    {article.Title}
+                    </Link>
+              </CardTitle>
+              </CardHeader>
+              <CardContent className="flex gap-2">
+                <p>{article.tag_1}</p>
+                <Separator orientation="vertical"/>
+                <p>{article.tag_2}</p>
+                <Separator orientation="vertical" />
+                <p>{article.tag_3}</p>
+              </CardContent>
+          </Card>
             </li>
           )
           )}
